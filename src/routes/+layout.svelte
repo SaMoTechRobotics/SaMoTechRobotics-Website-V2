8b6d37d23840a590c6a969f9e222946986b6f60c
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-12 12:22:36 -0700

    Added layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { page } from "$app/stores";
+  import Navbar from "../components/Navigation/Navbar.svelte";
+</script>
+
+<Navbar page={$page.url.pathname.replace("/", "") || "home"} />
+
+<slot />
+
+<style>
+  :root {
+    --bg-primary: rgb(239, 253, 255) 000;
+    --bg-secondary: #f5f5f5;
+
+    --primary: rgba(2, 145, 184, 1);
+    --secondary: rgba(154, 218, 244, 1);
+
+    --text: #272727;
+    --text-muted: #626262;
+  }
+</style>
