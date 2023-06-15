271c2ddf87c22d998b6a3efbf33bf3e8b40a8da7
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-15 08:30:31 -0700

    Added LinkButton

---
+++ src/components/Interactive/LinkButton.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let to: string;
+</script>
+
+<a href={to}>
+  <button>
+    <slot />
+  </button>
+</a>
+
+<style>
+  button {
+    margin: 0.5rem;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    border: none;
+    background-color: rgba(0, 0, 0, 0.2);
+    color: var(--text-inv);
+    font-size: 1rem;
+    font-weight: 600;
+    cursor: pointer;
+
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+    transition: background-color 300ms ease-in-out;
+  }
+
+  button:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+</style>
