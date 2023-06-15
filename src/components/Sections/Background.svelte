e67a37eeb182878ae96def5adc876a08eb83c012
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-15 08:31:22 -0700

    Added Background

---
+++ src/components/Sections/Background.svelte
@@ -0,0 +1,35 @@
+<div class="background" />
+
+<style>
+  .background {
+    background: linear-gradient(
+      45deg,
+      var(--dark) 0%,
+      var(--primary) 50%,
+      rgb(63, 207, 255) 75%,
+      rgb(53, 242, 255) 100%
+    );
+    background-size: 200% 200%;
+    background-attachment: fixed;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+
+    animation: bg-gradient 10s ease-in-out infinite;
+  }
+
+  @keyframes bg-gradient {
+    0% {
+      background-position: 100% 50%;
+    }
+    50% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 100% 50%;
+    }
+  }
+</style>
