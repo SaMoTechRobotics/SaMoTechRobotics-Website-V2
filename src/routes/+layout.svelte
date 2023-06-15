f8a0e25ff3d778a2946f661e8a70c4f6149a4466
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-15 08:31:41 -0700

    Added more root vars to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,24 @@
 <slot />
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Secular+One&display=swap");
+
   :root {
-    --bg-primary: rgb(239, 253, 255) 000;
+    --bg: rgb(239, 253, 255);
     --bg-secondary: #f5f5f5;
 
-    --primary: rgba(2, 145, 184, 1);
+    --primary: rgba(0, 200, 255, 1);
+    --dark: rgba(2, 145, 184, 1);
     --secondary: rgba(154, 218, 244, 1);
 
-    --text: #272727;
+    --text: #434343;
+    --text-inv: #fff;
     --text-muted: #626262;
+
+    --text-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
+    --box-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
+    --box-shadow-hover: 0 3px 3px rgba(0, 0, 0, 0.5);
+
+    --font: "Secular One", sans-serif;
   }
 </style>
