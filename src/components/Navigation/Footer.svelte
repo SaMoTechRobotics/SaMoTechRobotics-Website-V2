05332e87dab873d2282ac24e24c0629ebec507cd
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-16 10:32:10 -0700

    Added more theme vars and added footer

---
+++ src/components/Navigation/Footer.svelte
@@ -0,0 +1,36 @@
+<script>
+  import SocialButton from "../Interactive/SocialButton.svelte";
+</script>
+
+<div class="wrapper">
+  <div class="social">
+    <SocialButton to="" icon="akar-icons:instagram-fill" />
+    <SocialButton to="" icon="akar-icons:github-fill" />
+    <SocialButton to="" icon="akar-icons:youtube-fill" />
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    background-color: var(--bg);
+    color: var(--text-inv);
+
+    padding: 2rem 2rem;
+    height: 5rem;
+    width: calc(100% - 4rem);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .social {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    :global(> a) {
+      margin: 0 0.5rem;
+    }
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,44 @@
 <script>
   import { page } from "$app/stores";
+  import Footer from "../components/Navigation/Footer.svelte";
   import Navbar from "../components/Navigation/Navbar.svelte";
 </script>
 
 <Navbar page={$page.url.pathname.replace("/", "") || "home"} />
 
-<slot />
+<div class="content">
+  <slot />
+</div>
+
+<Footer />
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Secular+One&display=swap");
 
   :root {
     --bg: rgb(239, 253, 255);
+    --bg-hover: rgb(239, 248, 251);
     --bg-secondary: #f5f5f5;
 
     --primary: rgba(0, 200, 255, 1);
     --dark: rgba(2, 145, 184, 1);
     --secondary: rgba(154, 218, 244, 1);
 
-    --text: #434343;
+    --text: rgb(4, 112, 141);
+    --text-muted: rgb(102, 171, 192);
     --text-inv: #fff;
-    --text-muted: #626262;
 
     --text-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
+    --text-shadow-hover: 0 3px 3px rgba(0, 0, 0, 0.3);
     --box-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
     --box-shadow-hover: 0 3px 3px rgba(0, 0, 0, 0.5);
 
+    --img-shadow: 0 3px 3px rgba(0, 0, 0, 0.3);
+
     --font: "Secular One", sans-serif;
+  }
+
+  .content {
+    min-height: calc(100vh - 6rem - 5rem - 4rem);
   }
 </style>
