b779e07f13a1c77694a3e296a4f78017fa230dd2
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-16 11:05:43 -0700

    Applied new style to link button

--- src/components/Interactive/LinkButton.svelte
+++ src/components/Interactive/LinkButton.svelte
@@ -9,23 +9,32 @@
 </a>
 
 <style lang="scss">
-  button {
-    margin: 0.5rem;
-    padding: 0.5rem 1rem;
-    border-radius: 0.5rem;
-    border: none;
-    background-color: rgba(0, 0, 0, 0.2);
-    color: var(--text-inv);
-    font-size: 1rem;
-    font-weight: 600;
-    cursor: pointer;
+  a {
+    button {
+      margin: 0.5rem 0.5rem;
+      padding: 0.5rem 1rem;
+      border-radius: 0.5rem;
 
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      left: 50%;
 
-    transition: background-color 300ms ease-in-out;
+      background-color: transparent;
+      border: 3px outset var(--primary);
 
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.3);
+      color: var(--text);
+      font-size: 1rem;
+      font-weight: 600;
+
+      cursor: pointer;
+
+      box-shadow: var(--box-shadow);
+
+      transition: box-shadow 300ms ease-in-out,
+        background-color 300ms ease-in-out;
+
+      &:hover {
+        background-color: var(--bg-hover);
+        box-shadow: var(--box-shadow-hover);
+      }
     }
   }
 </style>
