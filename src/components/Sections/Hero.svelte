ec7b3253d2f9daacc039782d09c7bf1e3fa0a81f
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-16 11:05:29 -0700

    Fixed hero in mobile view

--- src/components/Sections/Hero.svelte
+++ src/components/Sections/Hero.svelte
@@ -15,6 +15,10 @@
     justify-content: center;
     align-items: center;
 
+    @media (max-width: 800px) {
+      padding: 0rem 0.1rem;
+      padding-top: 2rem;
+    }
     img {
       width: 100%;
       max-height: calc(100vh - 6rem - 5rem);
