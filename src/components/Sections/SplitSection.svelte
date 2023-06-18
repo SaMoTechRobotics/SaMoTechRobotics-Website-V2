241ec7def0d8722c27025191e5eae0955f5cb539
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-17 20:28:10 -0700

    Made background optional on section and fixed mobile layout

--- src/components/Sections/SplitSection.svelte
+++ src/components/Sections/SplitSection.svelte
@@ -2,21 +2,29 @@
   export let title: string;
   export let img: string;
   export let textSide: "left" | "right" = "right";
+  export let noBackground: "true" | "false" = "false";
 </script>
 
-<div class="content" class:right={textSide === "right"}>
+<div
+  class="content"
+  class:right={textSide === "right"}
+  class:noBackground={noBackground === "true"}
+>
   <div class="card">
     <h1>{title}</h1>
     <p>
       <slot />
     </p>
-    <slot class="buttons" name="buttons" />
+    <div class="buttons">
+      <slot name="buttons" />
+    </div>
   </div>
   <img src={img} alt="" />
 </div>
 
 <style lang="scss">
   .content {
+    background-color: var(--bg);
     display: grid;
     grid-template-columns: 1fr 1.5fr;
     grid-template-rows: 1fr;
@@ -24,9 +32,16 @@
     height: 100%;
 
     font-family: var(--font);
+    color: var(--text);
 
     padding: 4rem 2rem;
     padding-bottom: 0;
+
+    &.noBackground {
+      background-color: transparent;
+      color: var(--text-inv);
+      text-shadow: var(--text-shadow);
+    }
 
     &.right {
       grid-template-columns: 1.5fr 1fr;
@@ -42,19 +57,18 @@
 
     @media (max-width: 980px) {
       grid-template-columns: 1fr;
-      grid-template-rows: 1fr 1fr;
+      grid-template-rows: 1fr;
+      &.right {
+        grid-template-columns: 1fr;
+      }
 
       > .card {
-        order: 1;
+        order: 2;
       }
 
       > img {
-        order: -1;
+        order: 1;
       }
-    }
-
-    @media (max-width: 550px) {
-      grid-template-rows: 1fr 0.4fr;
     }
 
     .card {
@@ -63,9 +77,6 @@
       flex-direction: column;
       justify-content: center;
       align-items: center;
-
-      color: var(--text-inv);
-      text-shadow: var(--text-shadow);
 
       h1 {
         font-size: 2rem;
@@ -78,12 +89,15 @@
         text-align: center;
       }
 
-      > .buttons {
+      .buttons {
         margin-top: 1rem;
+
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-items: center;
+
+        text-align: center;
       }
     }
 
@@ -93,7 +107,7 @@
       object-fit: cover;
       border-radius: 1rem;
 
-      box-shadow: var(--box-shadow);
+      box-shadow: var(--img-shadow);
     }
   }
 </style>
