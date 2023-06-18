44c8ca34b60fb1b84ef75592d36e761d0a8a759e
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-17 20:29:12 -0700

    Made home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,99 @@
-Home
+<script>
+  import LinkButton from "../components/Interactive/LinkButton.svelte";
+  import Background from "../components/Sections/Background.svelte";
+  import Hero from "../components/Sections/Hero.svelte";
+  import SplitSection from "../components/Sections/SplitSection.svelte";
+  import Stat from "../components/Sections/Stats/Stat.svelte";
+  import StatsBar from "../components/Sections/Stats/StatsBar.svelte";
+</script>
+
+<Background />
+
+<Hero img="images/photos/team/samotech-team1.jpg" />
+
+<!-- <SectionBackground> -->
+<StatsBar>
+  <Stat
+    title="Schools Represented"
+    icon="material-symbols:school-outline"
+    value="2"
+  />
+  <Stat title="Team Members" icon="pepicons-pencil:people" value="9" />
+  <Stat title="Top Score" icon="lucide:medal" value="210" />
+  <Stat title="Ranking" icon="lucide:medal" value="4" />
+</StatsBar>
+
+<SplitSection
+  title="About Us"
+  img="images/photos/team/samotech-team2.jpg"
+  textSide="left"
+>
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge.
+  <div slot="buttons">
+    <LinkButton to="/about">Learn More</LinkButton>
+    <LinkButton to="/donate">Donate</LinkButton>
+  </div>
+</SplitSection>
+<!-- </SectionBackground> -->
+
+<SplitSection
+  title="FIRST Tech Challenge"
+  img="images/photos/samotech-winning1.webp"
+  textSide="right"
+>
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  SaMoTech robotics is a community based robotics team that participates in the
+  FIRST Tech Challenge. SaMoTech robotics is a community based robotics team
+  that participates in the FIRST Tech Challenge. SaMoTech robotics is a
+  community based robotics team that participates in the FIRST Tech Challenge.
+  <div slot="buttons">
+    <LinkButton to="/about">Learn More</LinkButton>
+  </div>
+</SplitSection>
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+<style>
+</style>
