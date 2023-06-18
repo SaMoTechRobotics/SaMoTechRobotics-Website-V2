390070a8d39065da2b14ce09eb16f1bd3aec65a7
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-17 20:26:20 -0700

    Added Profiles components

---
+++ src/components/Sections/About/Profile.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  export let name: string;
+
+  let img: string = `images/photos/bios/${name}.webp`;
+</script>
+
+<div class="wrapper">
+  <img src={img} alt="" />
+  <div class="content">
+    <h1>{name}</h1>
+    <hr />
+    <p>
+      <slot />
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    padding: 1rem 1rem;
+
+    display: grid;
+    grid-template-columns: 13rem 1fr;
+    grid-template-rows: 1fr;
+
+    img {
+      width: 12rem;
+      height: 12rem;
+      border-radius: 50%;
+      object-fit: cover;
+
+      box-shadow: var(--img-shadow);
+    }
+
+    .content {
+      padding: 1rem;
+
+      font-family: var(--font);
+      color: var(--text);
+
+      h1 {
+        font-size: 2rem;
+        margin-top: -0.5rem;
+        margin-bottom: 0;
+      }
+
+      hr {
+        width: 4rem;
+        margin: 0.5rem 0;
+        color: var(--text);
+      }
+
+      p {
+        font-size: 1rem;
+        font-weight: normal;
+        color: var(--text);
+      }
+    }
+  }
+</style>
---
+++ src/components/Sections/About/TeamProfiles.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+  import PreviousTeamProfiles from "./PreviousTeamProfiles.svelte";
+
+  const years = ["2023-2024", "2022-2023"] as const;
+  let year: (typeof years)[number] = "2023-2024";
+</script>
+
+<div class="wrapper">
+  <div class="top-content">
+    <!-- <Select options={years.join(",")} bind:selectedOption={year} /> -->
+    <select bind:value={year}>
+      {#each years as y}
+        <option value={y}>{y}</option>
+      {/each}
+    </select>
+  </div>
+  <div class="content">
+    <h1>SaMoTech Team of {year}</h1>
+    <img src={`images/photos/team/team${year}.jpg`} alt="" />
+    <div class="profiles">
+      {#if year === "2023-2024"}
+        <slot />
+      {:else if year === "2022-2023"}
+        <PreviousTeamProfiles {year} />
+      {/if}
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    background-color: var(--bg);
+    margin: 3rem 1rem;
+    border-radius: 1rem;
+    padding: 1rem;
+
+    font-family: var(--font);
+    color: var(--text);
+
+    box-shadow: var(--box-shadow);
+
+    .top-content {
+      padding: 1rem;
+      padding-bottom: 0;
+      display: flex;
+      justify-content: flex-end;
+
+      select {
+        background-color: transparent;
+        border: none;
+        font-size: 1rem;
+        cursor: pointer;
+
+        font-family: var(--font);
+        color: var(--text);
+
+        transition: color 100ms ease-in-out;
+
+        &:hover {
+          color: var(--dark);
+        }
+      }
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      img {
+        width: 50%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 1rem;
+        box-shadow: var(--img-shadow);
+      }
+
+      h1 {
+        margin-top: 0;
+        font-size: 2rem;
+      }
+
+      .profiles {
+        padding-top: 2rem;
+        display: grid;
+        grid-template-columns: repeat(2, minmax(300px, 1fr));
+        grid-gap: 1rem;
+      }
+    }
+  }
+</style>
