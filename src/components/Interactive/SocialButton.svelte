59b39b103c066a3b3588b9577442ff25a7d15bd1
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-06-18 10:00:56 -0700

    Added social button

---
+++ src/components/Interactive/SocialButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+
+  export let to: string;
+  export let icon: string;
+  export let size: string = "2";
+</script>
+
+<a href={to} target="_blank">
+  <div>
+    <Icon {icon} width={`${size}rem`} height={`${size}rem`} />
+  </div>
+</a>
+
+<style lang="scss">
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: var(--dark);
+    transition: color 300ms ease-in-out;
+
+    div {
+      transition: transform 200ms ease-in-out;
+    }
+
+    &:hover {
+      div {
+        transform: translateY(-0.2rem);
+      }
+    }
+  }
+</style>
