016f04fbf247372799c71845ebfe714f569d1ac1
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-15 07:02:34 -0700

    Fixed mobile spacing

--- src/components/Sections/Sponsors/SponsorsSection.svelte
+++ src/components/Sections/Sponsors/SponsorsSection.svelte
@@ -1,21 +1,24 @@
 <script>
-  import Sponsor from "./Sponsor.svelte";
+    import Sponsor from "./Sponsor.svelte";
 </script>
 
 <div class="wrapper">
-  <div class="content">
-    <h1>Our Sponsors</h1>
-    <p>
-      We would like to thank the main sponsor of our rookie year, ZipRecruiter, along with supporting sponsors, Rowe Neurology Institute and Panasonic for their generous support. Your contributions made SaMoTech possible and have allowed us to pursue our interests in robotics and engineering. We greatly appreciate your belief in us and the opportunities you have provided.
-    </p>
-  </div>
-  <Sponsor to="https://www.ziprecruiter.com/" sponsor="ZipRecruiter" />
-  <div class="sponsors">
-    <Sponsor to="https://www.na.panasonic.com/" sponsor="Panasonic" />
-    <Sponsor to="https://www.neurokc.com/" sponsor="RNI" />
-<!--    <Sponsor to="https://www.gobilda.com/" sponsor="goBilda" />-->
-    <div />
-  </div>
+    <div class="content">
+        <h1>Our Sponsors</h1>
+        <p>
+            We would like to thank the main sponsor of our rookie year, ZipRecruiter, along with supporting sponsors,
+            Rowe Neurology Institute and Panasonic for their generous support. Your contributions made SaMoTech possible
+            and have allowed us to pursue our interests in robotics and engineering. We greatly appreciate your belief
+            in us and the opportunities you have provided.
+        </p>
+    </div>
+    <Sponsor to="https://www.ziprecruiter.com/" sponsor="ZipRecruiter"/>
+    <div class="sponsors">
+        <Sponsor to="https://www.na.panasonic.com/" sponsor="Panasonic"/>
+        <Sponsor to="https://www.neurokc.com/" sponsor="RNI"/>
+        <!--    <Sponsor to="https://www.gobilda.com/" sponsor="goBilda" />-->
+        <div/>
+    </div>
 </div>
 
 <style lang="scss">
@@ -58,7 +61,7 @@
 
       @media (max-width: 800px) {
         grid-template-columns: repeat(1, 1fr);
-        grid-template-rows: repeat(4, 1fr);
+        grid-template-rows: repeat(2, 1fr);
       }
     }
   }
