1194e2caa59d8a44935f57fac8e39d172710439c
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-15 06:59:31 -0700

    Added icons for mobile

--- src/components/Navigation/Navbar.svelte
+++ src/components/Navigation/Navbar.svelte
@@ -1,70 +1,96 @@
 <script lang="ts">
-  export let page: string;
-  export let home: boolean;
+    import Icon from "@iconify/svelte";
 
-  let menuOpen = false;
+    export let page: string;
+    export let home: boolean;
 
-  let scrollY = 0;
+    let menuOpen = false;
+
+    let scrollY = 0;
+
+    let innerWidth = 0;
+    let icons = false;
+
+    $: icons = innerWidth < 550;
+
 </script>
 
 <title>SaMoTech Robotics » {page.charAt(0).toUpperCase() + page.slice(1)}</title
 >
 <div class="navbar-wrapper" class:scrolled={scrollY > 30} class:home={home}>
-  <div class="navbar-content">
-    <div class="left-items">
-      <a href="/"
-        ><img src="/images/logos/samotech-logo.png" alt="" width="50" />
-        <span>SaMoTech Robotics</span>
-      </a>
-    </div>
-    <div class="right-items">
-      <nav>
-        <a href="/">
-          <button class:active={page === "home"}>Home</button>
-        </a>
-        <a href="/about">
-          <button class:active={page === "about"}>About</button>
-        </a>
-<!--        <a href="/robot">-->
-<!--          <button class:active={page === "robot"}>Robot</button>-->
-<!--        </a>-->
-        <a href="/support">
-          <button class:active={page === "support"}>Support</button>
-        </a>
-      </nav>
-    </div>
-    <div class="right-menu">
-      <div class="menu" class:hidden={!menuOpen}>
-        <div class="menu-items">
-          <a href="/">
-            <button class:active={page === "home"}>Home</button>
-          </a>
-          <a href="/about">
-            <button class:active={page === "about"}>About</button>
-          </a>
-<!--          <a href="/robot">-->
-<!--            <button class:active={page === "robot"}>Robot</button>-->
-<!--          </a>-->
-          <a href="/support">
-            <button class:active={page === "support"}>Support</button>
-          </a>
+    <div class="navbar-content">
+        <div class="left-items">
+            <a href="/"
+            ><img src="/images/logos/samotech-logo.png" alt="" width="50"/>
+                <span>SaMoTech Robotics</span>
+            </a>
         </div>
-      </div>
-      <button
-        class="hamburger"
-        on:click={() => (menuOpen = !menuOpen)}
-        class:open={menuOpen}
-      >
-        <span />
-        <span />
-        <span />
-      </button>
+        <div class="right-items">
+            <nav>
+                <a href="/">
+                    <button class:active={page === "home"}>
+                        {#if !icons}
+                            Home
+                        {:else}
+                            <Icon icon="ion:home" class="nav-icon"/>
+                        {/if}
+                    </button>
+                </a>
+                <a href="/about">
+                    <button class:active={page === "about"}>
+                        {#if !icons}
+                            About
+                        {:else}
+                            <Icon icon="mdi:account-group" class="nav-icon"/>
+                        {/if}
+                    </button>
+                </a>
+                <!--        <a href="/robot">-->
+                <!--          <button class:active={page === "robot"}>Robot</button>-->
+                <!--        </a>-->
+                <a href="/support">
+                    <button class:active={page === "support"}>
+                        {#if !icons}
+                            Support
+                        {:else}
+                            <Icon icon="mdi:heart" class="nav-icon"/>
+                        {/if}
+                    </button>
+                </a>
+            </nav>
+        </div>
+        <!--    <div class="right-menu">-->
+        <!--      <div class="menu" class:hidden={!menuOpen}>-->
+        <!--        <div class="menu-items">-->
+        <!--          <a href="/">-->
+        <!--            <button class:active={page === "home"}>Home</button>-->
+        <!--          </a>-->
+        <!--          <a href="/about">-->
+        <!--            <button class:active={page === "about"}>About</button>-->
+        <!--          </a>-->
+        <!--&lt;!&ndash;          <a href="/robot">&ndash;&gt;-->
+        <!--&lt;!&ndash;            <button class:active={page === "robot"}>Robot</button>&ndash;&gt;-->
+        <!--&lt;!&ndash;          </a>&ndash;&gt;-->
+        <!--          <a href="/support">-->
+        <!--            <button class:active={page === "support"}>Support</button>-->
+        <!--          </a>-->
+        <!--        </div>-->
+        <!--      </div>-->
+        <!--      <button-->
+        <!--        class="hamburger"-->
+        <!--        on:click={() => (menuOpen = !menuOpen)}-->
+        <!--        class:open={menuOpen}-->
+        <!--      >-->
+        <!--        <span />-->
+        <!--        <span />-->
+        <!--        <span />-->
+        <!--      </button>-->
+        <!--    </div>-->
     </div>
-  </div>
 </div>
-<div class="padding" class:home={home} />
+<div class="padding" class:home={home}/>
 
-<svelte:window bind:scrollY />
+<svelte:window bind:scrollY bind:innerWidth/>
 
 <style lang="scss">
   .padding {
@@ -84,12 +110,12 @@
     font-family: var(--font);
     transition: background-color 0ms ease-in-out, box-shadow 300ms ease-in-out;
 
-&.home {
-  background-color: rgba(0, 200, 255, 1);
-}
+    &.home {
+      background-color: rgba(0, 200, 255, 1);
+    }
 
     &.scrolled {
-    background-color: rgba(0, 200, 255, 1);
+      background-color: rgba(0, 200, 255, 1);
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
   }
@@ -197,7 +223,7 @@
         left: 0;
 
         transition: transform 200ms ease-in-out, opacity 200ms ease-in-out,
-          top 200ms ease-in-out;
+        top 200ms ease-in-out;
 
         &:nth-child(1) {
           top: 0.3rem;
@@ -283,19 +309,19 @@
     }
   }
 
-  @media (min-width: 550px) {
-    .right-menu {
-      display: none;
-    }
-  }
-
-  @media (max-width: 550px) {
-    .right-items {
-      display: none;
-    }
-
-    .right-menu {
-      display: block;
-    }
-  }
+  //@media (min-width: 550px) {
+  //  .right-menu {
+  //    display: none;
+  //  }
+  //}
+  //
+  //@media (max-width: 550px) {
+  //  .right-items {
+  //    display: none;
+  //  }
+  //
+  //  .right-menu {
+  //    display: block;
+  //  }
+  //}
 </style>
