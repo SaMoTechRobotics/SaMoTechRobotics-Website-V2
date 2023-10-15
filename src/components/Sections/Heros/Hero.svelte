43ce7a09f36d81f50e7cc55a8d2f44c935d5bfb7
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-10-14 20:26:22 -0700

    Added parallax effect

--- src/components/Sections/Heros/Hero.svelte
+++ src/components/Sections/Heros/Hero.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+    import {onMount} from "svelte";
+
     export let img: string;
+
+    let y = 0;
+
+    onMount(() => {
+        window.addEventListener('scroll', () => {
+            y = window.scrollY;
+        });
+    });
 </script>
 
-<div class="wrapper">
-    <img src={img} alt=""/>
+<div class="wrapper" style="background-image: url({img}); background-position-y: {-y / 3}px;">
+    <!--    <img src={img} alt=""/>-->
     <div class="overlay">
         <slot/>
     </div>
@@ -18,11 +28,17 @@
     width: 100vw;
     height: 100vh;
 
+    margin-bottom: -0.5rem;
+
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+
     img {
       width: 100%;
       height: 100%;
 
-      margin-bottom: -10px;
 
       object-fit: cover;
     }
